1e8eccaadebcab4b03c489d0a154822c619630ea
Frederick Z. <echaya@gmail.com>
2025-07-09 20:12:41 +0800

    chore: bump the version to v0.3.0

--- lua/neowiki/init.lua
+++ lua/neowiki/init.lua
@@ -15,7 +15,7 @@ local markdown_patterns = {
 
 --- Public API ---
 
-M.VERSION = "0.2.0"
+M.VERSION = "0.3.0"
 M.open_wiki = api.open_wiki
 M.open_wiki_new_tab = api.open_wiki_new_tab
 M.open_wiki_floating = api.open_wiki_floating
